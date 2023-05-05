5d1cf977b74555b2aaec3b43c43ff4d84df6c1fa
MrChif <74892009+MrChif@users.noreply.github.com>
2023-05-05 01:08:40 -0600

    Add files via upload

--- generate_random_player_data_mcc_in_csv.py
+++ generate_random_player_data_mcc_in_csv.py
@@ -4,41 +4,54 @@ import csv
 
 from select_random_team_name import select_random_team_name
 
-seed_columns = 6
-seed_rows = random.randint(1,25)
-header = ['Team Placement', 'Team Coins', 'Individual Placement', 'Individual Coins', 'Gets to Dodgebolt?', 'Wins Dodgebolt?']
+seed_columns = 8
+seed_rows = random.randint(1,30)
+header = ['Team','Team Placement', 'Team Coins', 'Individual Placement', 'Individual Coins', 'Gets to Dodgebolt?', 'Wins Dodgebolt?','Canonicity']
 data = []
+
+def convert_to_string(text_list):
+	t_string = ''
+	for i in text_list:
+		t_string += '' + i
+	return t_string.strip()
+
+def select_team(item):
+	team_as_list = select_random_team_name()
+	item.append(convert_to_string(team_as_list))
 
 def add_team_coins(item):
 	if 7 <= item <= 10:
-		data[1].append(random.randint(6000,12332))
+		data[2].append(random.randint(6000,12332))
 	if 4 <= item <= 6:
-		data[1].append(random.randint(12333,18665))
+		data[2].append(random.randint(12333,18665))
 	if 1 <= item <= 3:
-		data[1].append(random.randint(18666,25000))
-
+		data[2].append(random.randint(18666,25000))
 
 def add_team_placement(item):
 	item.append(random.randint(1,10))
 
 def add_individual_coins(item):
 	if 27 <= item <= 40:
-		data[3].append(random.randint(300,1600))
+		data[4].append(random.randint(300,1600))
 	if 13 <= item <= 26:
-		data[3].append(random.randint(1601,2900))
+		data[4].append(random.randint(1601,2900))
 	if 1 <= item <= 12:
-		data[3].append(random.randint(2901,4200))
+		data[4].append(random.randint(2901,4200))
 
 def add_individual_placement(item):
 	item.append(random.randint(1,40))
 
 def dogdebolt_yes():
-		data[4].append(1)
-		data[5].append(random.randint(0,1))
+		data[5].append(1)
+		data[6].append(random.randint(0,1))
 
 def dodgebolt_no():
-		data[4].append(0)
 		data[5].append(0)
+		data[6].append(0)
+
+def canon(item):
+	canon_options = ["Canon","Non-Canon","Half-Canon"]
+	item.append(convert_to_string(random.choices(canon_options,weights=(90,7,3))))
 
 def create_data(data,seed_rows,seed_columns):
 	num_list = 0
@@ -47,22 +60,27 @@ def create_data(data,seed_rows,seed_columns):
 		num_list += 1
 		for i in range(seed_rows):
 			if num_list == 1:
+				select_team(item_for_data)
+			if num_list == 2:
 				add_team_placement(item_for_data)
-			if num_list == 3:
+			if num_list == 4:
 				add_individual_placement(item_for_data)
+			if num_list == 8:
+				canon(item_for_data)
 		data.append(item_for_data)
 
-	for item in data[0]:
+	for item in data[1]:
 		if item == 1 or item == 2:
 			dogdebolt_yes()
 		else:
 			dodgebolt_no()
 		
-	for item in data[0]:
+	for item in data[1]:
 		add_team_coins(item)
 
-	for item in data[2]:
+	for item in data[3]:
 		add_individual_coins(item)
+	
 
 def transpose_data(data):
 	data_np = np.array(data)
--- select_random_team_name.py
+++ select_random_team_name.py
@@ -17,10 +17,6 @@ def select_random(teams_n,teams_h,teams_c):
     chosen_list = [teams_n[n_random],teams_h[h_random],teams_c[c_random]]
     #print(chosen_list)
     return random.choices(chosen_list,weights=(80,10,10))
-    
-def name_to_string(name):
-    string = ''
-    string.join(name)
 
 #main function from this file
 def select_random_team_name():
@@ -28,7 +24,7 @@ def select_random_team_name():
     teams_h = read_txt_file('name_of_teams_halloween.txt')
     teams_c = read_txt_file('name_of_teams_christmas.txt')
     selected_name = select_random(teams_n,teams_h,teams_c)
-    name_to_string(selected_name)
     return selected_name
 
-
+#test  = select_random_team_name()
+#print(test)
